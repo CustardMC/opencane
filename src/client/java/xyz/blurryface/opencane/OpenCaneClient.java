aaff4abd3497d02946fbcdb150fe499f87b26b51
blryface <124838505+blryface@users.noreply.github.com>
2024-02-27 16:10:41 -0300

    Mod Template

---
+++ src/client/java/xyz/blurryface/opencane/OpenCaneClient.java
@@ -0,0 +1,10 @@
+package xyz.blurryface.opencane;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OpenCaneClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
