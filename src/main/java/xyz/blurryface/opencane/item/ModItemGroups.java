bd495efb9a496b49f5c8eaa928575373801ded4e
blryface <124838505+blryface@users.noreply.github.com>
2024-03-02 23:58:30 -0300

    do stuff (why is it not mergeable by default?) (#3)
    * i forgor
    
    * Add seeds and saplings for weed, ccane and coffee
    
    * Change the way items get registered, and... a few other stuff
    
    ---------
    
    Co-authored-by: blryface <blurry@pridecraft.gay>

--- src/main/java/xyz/blurryface/opencane/item/ModItemGroups.java
+++ src/main/java/xyz/blurryface/opencane/item/ModItemGroups.java
@@ -17,9 +17,73 @@ public class ModItemGroups {
     public static final ItemGroup CANE_GROUP = Registry.register(Registries.ITEM_GROUP,
             new Identifier(OpenCane.MOD_ID, "oc_cane"),
             FabricItemGroup.builder().displayName(Text.translatable("oc_cane.opencane"))
+                    .icon(() -> new ItemStack(ModItems.CCANE)).entries((displayContext, entries) -> {
+
+                    entries.add(ModItems.CCANE);
+                    entries.add(ModItems.LEAVES);
+                    entries.add(ModItems.DRIED_LEAVES);
+
+                    }).build());
+    public static final ItemGroup SEEDS_GROUP = Registry.register(Registries.ITEM_GROUP,
+            new Identifier(OpenCane.MOD_ID, "oc_seeds"),
+            FabricItemGroup.builder().displayName(Text.translatable("oc_seeds.opencane"))
+                    .icon(() -> new ItemStack(ModItems.SEEDS)).entries((displayContext, entries) -> {
+
+                        entries.add(ModItems.SEEDS);
+                        entries.add(ModItems.COFFEE_SEEDS);
+                        entries.add(ModBlocks.WEED_SAPLING);
+
+                    }).build());
+
+    public static final ItemGroup LEAF_GROUP = Registry.register(Registries.ITEM_GROUP,
+            new Identifier(OpenCane.MOD_ID, "oc_leaves"),
+            FabricItemGroup.builder().displayName(Text.translatable("oc_leaves.opencane"))
+                    .icon(() -> new ItemStack(Items.FERN)).entries((displayContext, entries) -> {
+
+                        // entries.add(ModBlocks.PLANT); <-- is a comment because you shouldn't be able to place crop blocks directly, you do so via seeds.
+                        entries.add(ModBlocks.WILD_PLANT);
+                        // entries.add(ModBlocks.WEED_PLANT); <-- is a comment because you shouldn't be able to place crop blocks directly, you do so via seeds.
+                        entries.add(ModBlocks.WILD_WEED_PLANT);
+                        // entries.add(ModBlocks.COFFEE_PLANT); <-- is a comment because you shouldn't be able to place crop blocks directly, you do so via seeds.
+                        entries.add(ModBlocks.WILD_COFFEE_PLANT);
+
+                    }).build());
+
+    public static final ItemGroup SHROOMS_GROUP = Registry.register(Registries.ITEM_GROUP,
+            new Identifier(OpenCane.MOD_ID, "oc_shrooms"),
+            FabricItemGroup.builder().displayName(Text.translatable("oc_shrooms.opencane"))
+                    .icon(() -> new ItemStack(Items.BROWN_MUSHROOM)).entries((displayContext, entries) -> {
+
+                        entries.add(Blocks.RED_MUSHROOM);
+                        entries.add(Blocks.BROWN_MUSHROOM);
+
+                    }).build());
+
+    public static final ItemGroup DRINKS_GROUP = Registry.register(Registries.ITEM_GROUP,
+            new Identifier(OpenCane.MOD_ID, "oc_drinks"),
+            FabricItemGroup.builder().displayName(Text.translatable("oc_drinks.opencane"))
+                    .icon(() -> new ItemStack(Items.POTION)).entries((displayContext, entries) -> {
+
+                        entries.add(Items.POTION);
+
+                    }).build());
+
+    public static final ItemGroup EDIBLES_GROUP = Registry.register(Registries.ITEM_GROUP,
+            new Identifier(OpenCane.MOD_ID, "oc_edibles"),
+            FabricItemGroup.builder().displayName(Text.translatable("oc_edibles.opencane"))
+                    .icon(() -> new ItemStack(Items.COOKIE)).entries((displayContext, entries) -> {
+
+                        entries.add(Items.COOKIE);
+
+                    }).build());
+
+    public static final ItemGroup MISC_GROUP = Registry.register(Registries.ITEM_GROUP,
+            new Identifier(OpenCane.MOD_ID, "oc_misc"),
+            FabricItemGroup.builder().displayName(Text.translatable("oc_misc.opencane"))
+                    .icon(() -> new ItemStack(Blocks.COMMAND_BLOCK)).entries((displayContext, entries) -> {
                     .icon(() -> new ItemStack(Items.OAK_SAPLING)).entries((displayContext, entries) -> {
 
-                    entries.add(Blocks.OAK_LEAVES);
+                    entries.add(Items.DIRT);
 
                     }).build());
 
