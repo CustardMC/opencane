2632dffe4e7d51b6b433bc9b3aedddddfea6f234
blryface <blurry@pridecraft.gay>
2024-03-04 15:42:06 -0300

    datagen

---
+++ src/main/java/xyz/blurryface/opencane/datagen/ModLootTableProvider.java
@@ -0,0 +1,45 @@
+package xyz.blurryface.opencane.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+import xyz.blurryface.opencane.block.ModBlocks;
+import xyz.blurryface.opencane.block.custom.CcaneCropBlock;
+import xyz.blurryface.opencane.item.ModItems;
+import net.minecraft.block.Block;
+import net.minecraft.data.server.loottable.BlockLootTableGenerator;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.loot.LootTable;
+import net.minecraft.loot.condition.BlockStatePropertyLootCondition;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.entry.LeafEntry;
+import net.minecraft.loot.entry.LootPoolEntry;
+import net.minecraft.loot.function.ApplyBonusLootFunction;
+import net.minecraft.loot.function.SetCountLootFunction;
+import net.minecraft.loot.provider.number.UniformLootNumberProvider;
+import net.minecraft.predicate.StatePredicate;
+
+public class ModLootTableProvider extends FabricBlockLootTableProvider {
+    public ModLootTableProvider(FabricDataOutput dataOutput) {
+        super(dataOutput);
+    }
+
+    @Override
+    public void generate() {
+
+        BlockStatePropertyLootCondition.Builder builder = BlockStatePropertyLootCondition.builder(ModBlocks.PLANT).properties(StatePredicate.Builder.create()
+                .exactMatch(CcaneCropBlock.AGE, 5));
+        addDrop(ModBlocks.PLANT, cropDrops(ModBlocks.PLANT, ModItems.LEAVES, ModItems.SEEDS, builder));
+    }
+
+/*    public LootTable.Builder copperLikeOreDrops(Block drop, Item item) {
+        return BlockLootTableGenerator.dropsWithSilkTouch(drop, (LootPoolEntry.Builder)this.applyExplosionDecay(drop,
+                ((LeafEntry.Builder)
+                        ItemEntry.builder(item)
+                                .apply(SetCountLootFunction
+                                        .builder(UniformLootNumberProvider
+                                                .create(2.0f, 5.0f))))
+                        .apply(ApplyBonusLootFunction.oreDrops(Enchantments.FORTUNE))));
+    } */
+}
