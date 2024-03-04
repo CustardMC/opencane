b771e1d0307d00bdfdfbef79b1079b80786d2160
blryface <blurry@pridecraft.gay>
2024-03-04 15:28:28 -0300

    Plantable c-cane

--- src/client/java/xyz/blurryface/opencane/OpenCaneClient.java
+++ src/client/java/xyz/blurryface/opencane/OpenCaneClient.java
@@ -1,9 +1,15 @@
 package xyz.blurryface.opencane;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import xyz.blurryface.opencane.block.ModBlocks;
+import net.minecraft.client.render.RenderLayer;
 
 public class OpenCaneClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
+
+		BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.PLANT, RenderLayer.getCutout());
+
 	}
 }
